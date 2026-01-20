0b46c7c61868b0c040dc32b723484cd04e32ef93
Dana Aylin Garcia Bahena <20253ds134@utez.edu.mx>
2026-01-19 21:58:29 -0600

    Agregar calculo de IVA

--- GitConflictoGarciaDana/src/Main.java
+++ GitConflictoGarciaDana/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final double IVA=0.16;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
